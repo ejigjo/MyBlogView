c1abe611d0406cb44fc179cdc27a3d8b0a72efb3
ejigjo <ejigjo@hotmail.com>
2023-12-31 16:49:10 +0800

    dist

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base:"/travelBlog/",
   plugins: [
     vue(),
   ],
@@ -22,5 +23,5 @@ export default defineConfig({
         rewrite:(path)=>path.replace(/^\/api/,'')
       }
     }
-  }
+  },
 })
